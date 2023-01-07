73eaae07dc4a32c7a31f258d3cc28a7fb52dca4d
zenonet <78077158+zenonet@users.noreply.github.com>
2023-01-07 16:38:12 +0100

    Refactoring

--- NeopixelControl/MauiProgram.cs
+++ NeopixelControl/MauiProgram.cs
@@ -1,27 +1,27 @@
 ﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Neopixel.Client;
 using NeopixelControl.Data;
+using NeopixelControl.Shared;
 
 namespace NeopixelControl;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => { fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); });
 
-		builder.Services.AddMauiBlazorWebView();
-		#if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
-		
-		builder.Services.AddSingleton<WeatherForecastService>();
 
-		return builder.Build();
-	}
+        
+        builder.Services.AddScoped<NeopixelClient>(_ => new("192.168.1.157"));
+
+        return builder.Build();
+    }
 }
